301e25c42958f195c6f52dd6d4ff76b3dee942d9
Randall Morey <randall@randallmorey.com>
2021-03-18 08:45:43 -0400

    feature: add s3 redirects and a records

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.32.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  backend "remote" {
+    organization = "kwiniaskaridge"
+    workspaces {
+      name = "website-org"
+    }
+  }
+}
+
+data "terraform_remote_state" "zones" {
+  backend = "remote"
+  config = {
+    organization = "kwiniaskaridge"
+    workspaces = {
+      name = "dns"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+}
+
+module "redirects" {
+  for_each            = data.terraform_remote_state.zones.outputs.zones
+  source              = "./redirects"
+  zone                = each.value.hosted-zone
+  core-website-domain = "www.${data.terraform_remote_state.zones.outputs.zones["org"].hosted-zone.name}"
+}
