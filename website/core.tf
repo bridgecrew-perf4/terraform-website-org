b7fcd6eb9e8208676d1e9dee914ddc0a8d69f7e2
Randall Morey <randall@randallmorey.com>
2021-03-22 10:32:43 -0400

    fix: add required ttl field

--- website/core.tf
+++ website/core.tf
@@ -5,6 +5,7 @@ resource "aws_route53_record" "website" {
   zone_id = var.zone.zone_id
   name    = var.core-website-domain
   type    = "A"
+  ttl     = 14400
 
   records = [
     "kwiniaskaridge.github.io",
